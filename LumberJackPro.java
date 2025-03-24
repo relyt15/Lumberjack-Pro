98702630d46fd7c5f20f910cec9292be8b412827
nhang12 <nhang@oakland.edu>
2025-03-24 18:00:29 -0400

    Update LumberJackPro.java
    For the icon, you'll need to create a new (resources) folder in the src folder of the project.

--- LumberJackPro.java
+++ LumberJackPro.java
@@ -1,42 +1,60 @@
-import java.util.Scanner;
+package com.mycompany.lumberjack_pro;
+
+//import java.util.Scanner;
 import javafx.application.Application;
+import javafx.geometry.HPos;
 import javafx.scene.Scene;
 import javafx.scene.Group;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
 
-public class LumberjackPro extends Application {
+public class LumberJackPro extends Application {
     private BorderPane windowContainer;
-    private VBox taskContainer;
+    private HBox taskContainer;
     private HBox projectContainer;
     
     
     @Override
     public void start(Stage primaryStage) {
         windowContainer = new BorderPane();
-        taskContainer = new VBox(10);  // Container to hold task panes
-        projectContainer = new HBox(20); // Container to hold project panes
+        taskContainer = new HBox(10);  // Container to hold task panes
+        projectContainer = new HBox(20); // Container to hold project panes   
+     
+        
+        Font font = new Font(13); 
+        Label greeting = new Label("What Are Your Plans for Today?");
+        greeting.setFont(font);
+        GridPane.setHalignment (greeting, HPos.LEFT);
         
         Button addTaskButton = new Button("Add New Task");
         Button addProjectButton = new Button("Add New Project");
 
         addTaskButton.setOnAction(e -> addNewTask());  // Handle addTask button click
         addProjectButton.setOnAction (e -> addNewProject()); // handles addProject button click
-
         
-        VBox taskLayout = new VBox(10, addTaskButton, taskContainer);  // Overall layout
-        HBox projectLayout = new HBox(20, addProjectButton, projectContainer);
+        VBox taskLayout = new VBox(20, addTaskButton, taskContainer);  // Overall layout
+        HBox projectLayout = new HBox(30, addProjectButton, projectContainer);
         
         windowContainer.setBottom(taskLayout);
         windowContainer.setCenter(projectLayout);
         
-        Group root = new Group(windowContainer); //taskLayout, projectLayout
-        Scene scene = new Scene(root, 300, 400);
         
-        primaryStage.setTitle("Task Manager");
+        Group root = new Group(greeting, windowContainer); //taskLayout, projectLayout
+        Scene scene = new Scene(root, 1200, 600, Color.BEIGE);
+        
+        Image tree = new Image(getClass().getResourceAsStream("/LUMBER.png"));
+        primaryStage.getIcons().add(tree);
+        
+        primaryStage.setTitle("LumberJack Pro");
+        primaryStage.setResizable(false);
         primaryStage.setScene(scene);
         primaryStage.show();
     }
@@ -49,6 +67,7 @@ public class LumberjackPro extends Application {
         TaskPane taskPane = new TaskPane(newTask);
         taskContainer.getChildren().add(taskPane);
     }
+    
     private void addNewProject() {
         Project newProject = new Project();
         
