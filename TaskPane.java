c62dc9425aae520cb5364da4c3a730e90d4fc802
relyt15 <134524170+relyt15@users.noreply.github.com>
2025-04-12 11:35:38 -0400

    Update TaskPane.java

--- TaskPane.java
+++ TaskPane.java
@@ -1,4 +1,5 @@
 import java.time.LocalDate;
+import java.util.Optional;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
@@ -22,22 +23,23 @@ public class TaskPane extends HBox {
     private CheckBox completedCheckBox;
     private Button saveButton;
     private Label status;
+    private Button editTaskButton;
 
     public TaskPane(Task task) {
         this.task = task;
         TaskService taskServ = new TaskService();
         
         // Label for Task Name
-        nameLabel = new Label(task.getTaskName());
+        nameLabel = new Label("Task: " + task.getTaskName());
         nameLabel.setStyle("-fx-font-weight: bold;");
         nameLabel.setAlignment(Pos.CENTER);
         
         
         //Label for Due Date
-        dueDateLabel = new Label(task.getTaskDueDate());
+        dueDateLabel = new Label("Due Date: " + task.getTaskDueDate());
         
         // Label for task description
-        descriptionLabel = new Label(task.getTaskDescription());
+        descriptionLabel = new Label("Description: " + task.getTaskDescription());
         
 
         // Checkbox for Completion
@@ -52,6 +54,8 @@ public class TaskPane extends HBox {
         saveButton.setOnAction(e -> dueDateField.setEditable(false));
         saveButton.setOnAction(e -> nameField.setEditable(false));
 
+        editTaskButton = new Button("Edit");
+        editTaskButton.setOnAction(e -> editTaskDetails());
         
         
         // Update Task when checkbox is clicked
@@ -71,7 +75,7 @@ public class TaskPane extends HBox {
 
         // Add elements to the pane
         this.getChildren().addAll(nameLabel, descriptionLabel, dueDateLabel,
-                completedCheckBox);
+                completedCheckBox, editTaskButton);
     }
     
     private void saveTaskDetails() {
@@ -83,6 +87,21 @@ public class TaskPane extends HBox {
         else {
             task.setTaskStatus("Pending");
         }
+    }
+    
+    private void editTaskDetails() {
+        TaskService man1 = new TaskService();
+        
+        Optional<Task> result = man1.editTaskDialog(task);
+        result.ifPresent(updated -> {
+            // Update the local project reference
+            task = updated;
+
+            // Update UI labels
+            nameLabel.setText("Task: " + task.getTaskName());
+            dueDateLabel.setText("Due Date: " + task.getTaskDueDate());
+            descriptionLabel.setText("Description: " + task.getTaskDescription());
+        });
     }
 
 
